22e685fd66592ac18e91ccec6d9b4c8d846c2caa
Ricardo Lima <ricardolima5722@gmail.com>
2025-09-29 18:14:33 -0300

    Criação do projeto

---
+++ pedidos-backend/src/main/java/com/ricardo/pedidos_backend/PedidosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.pedidos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosBackendApplication.class, args);
+	}
+
+}
---
+++ pedidos-backend/src/test/java/com/ricardo/pedidos_backend/PedidosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.pedidos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
